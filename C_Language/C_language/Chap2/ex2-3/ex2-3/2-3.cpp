5daf4fc590e6aa8a4007c246262829f00df6895f
kimth007kim <kimth007kim@naver.com>
2020-12-17 01:38:05 +0900

    이것이 C언어다 2장 상수와 데이터 출력

---
+++ C_Language/C_language/Chap2/ex2-3/ex2-3/2-3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	printf("1234567890\n");
+	printf("Be happy\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
---
+++ C_Language/C_language/Chap2/ex2-6/ex2-6/2-6.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%.1lf\n", 1e6);
+	printf("%.7lf\n", 3.14e-5);
+
+	return 0;
+}
