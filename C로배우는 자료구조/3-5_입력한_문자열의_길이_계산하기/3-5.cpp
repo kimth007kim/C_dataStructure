72b49c7c2d05709321cc29462f96d84bdb4817c7
kimth007kim <kimth007kim@naver.com>
2021-01-27 18:43:29 +0900

    C로 배우는 자료구조 3-8까지 완료

---
+++ C로배우는 자료구조/3-5_입력한_문자열의_길이_계산하기/3-5.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main() {
+	int i, length = 0;
+	char str[50];
+	printf("\n 문자열을 입력하세요: ");
+	gets_s(str);
+	printf("\n 입력된 문자열은 \n \"");
+	for (i = 0; str[i]; i++) {
+		printf("%c", str[i]);
+		length += 1;
+	}
+
+	printf("\" \n입니다.");
+	printf("\n\n입력된 문자열의 길이 = %d \n", length);
+}
