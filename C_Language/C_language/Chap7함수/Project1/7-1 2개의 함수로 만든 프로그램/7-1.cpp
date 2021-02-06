96cdbece50ed8d3c98de42ed14b5190b325cf2d6
kimth007kim <kimth007kim@naver.com>
2021-02-07 00:19:31 +0900

    c언어 7장 함수 완료

---
+++ C_Language/C_language/Chap7함수/Project1/7-1 2개의 함수로 만든 프로그램/7-1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//int add(int x, int y);
+
+
+int main(void) {
+	int a = 10, b = 20;
+	int res;
+	res = add(a, b);
+	printf("result: %d\n", res);
+	return 0;
+}
+
+int add(int x, int y) {
+	int temp;
+	temp = x + y;
+	return temp;
+}
